0f55f90decb91b8a6cb12161ed7ddfd28a81a950
Mads Skov Schiellerup <mads.schiellerup@gmail.com>
2017-09-05 16:48:22 +0200

    dads

--- Viprover.py
+++ Viprover.py
@@ -12,18 +12,18 @@ print("Running...")
 print frindo.set_speed(100)
 print frindo.set_turnspeed(150)
 print frindo.set_step_time(2000)
-print frindo.set_turn_time(1500)
+print frindo.set_turn_time(1000)
 
 sleep(1)
 
 # request to read Front IR sensor (analog sensor 0)
-print "Front sensor = ", frindo.read_front_ir_sensor()
+#print "Front sensor = ", frindo.read_front_ir_sensor()
 
 # request to read Right IR sensor (analog sensor 1)
-print "Right sensor = ", frindo.read_right_ir_sensor()
+#print "Right sensor = ", frindo.read_right_ir_sensor()
 
 # request to read Left IR sensor (analog sensor 2)
-print "Left sensor = ", frindo.read_left_ir_sensor()
+#print "Left sensor = ", frindo.read_left_ir_sensor()
 
 
 
@@ -40,7 +40,7 @@ print "Left sensor = ", frindo.read_left_ir_sensor()
 #sleep(2)
 for x in range(1,5):
 	print frindo.right()
-	sleep(0.53)
+	sleep(1)
 	print frindo.stop()
 	sleep(1)
 
