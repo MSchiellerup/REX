d116370971603f728dda935f567e908dfaef9d7a
Mads Skov Schiellerup <mads.schiellerup@gmail.com>
2017-09-05 16:16:46 +0200

    blah

---
+++ ExerciseGo-5-9-17.py
@@ -0,0 +1,41 @@
+import time
+from time import sleep
+
+import robot
+
+# Create a robot object and initialize
+frindo = robot.Robot()
+
+print("Running...")
+
+# Set speed
+print frindo.set_speed(100)
+print frindo.set_turnspeed(150)
+print frindo.set_step_time(2000)
+print frindo.set_turn_time(1500)
+
+sleep(1)
+
+# Looping driving test 1
+print("First test drive, go-stop-right-stop")
+for x in range(1,5)
+	print("Action %d", x)
+	print frindo.go()
+	sleep(3)
+	print frindo.stop()
+	sleep(2)
+	print frindo.right()
+	sleep(2)
+	print frindo.stop()
+
+# Looping driving test 2
+print("Second test drive, ")
+for x in range(1,5)
+	print("Action %d", x)
+	print frindo.go()
+	sleep(3)
+	print frindo.stop()
+	sleep(2)
+	print frindo.step_rotate_right()
+	sleep(2)
+	print frindo.stop()
