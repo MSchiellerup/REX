55bdc0b19b19e7ad81852b08f8eb02265a589f43
Mads Skov Schiellerup <mads.schiellerup@gmail.com>
2017-09-08 10:50:00 +0200

    dads

--- Viprover.py
+++ Viprover.py
@@ -10,7 +10,7 @@ print("Running...")
 
 # Set speed
 print frindo.set_speed(70)
-print frindo.set_turnspeed(130)
+print frindo.set_turnspeed(117)
 print frindo.set_step_time(1200)
 print frindo.set_turn_time(1000)
 
