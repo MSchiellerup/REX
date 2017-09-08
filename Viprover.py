1839c9566c515d4abfefa4281dd54d8aa10eafbc
Mads Skov Schiellerup <mads.schiellerup@gmail.com>
2017-09-08 10:43:36 +0200

    dads

--- Viprover.py
+++ Viprover.py
@@ -10,13 +10,13 @@ print("Running...")
 
 # Set speed
 print frindo.set_speed(70)
-print frindo.set_turnspeed(110)
+print frindo.set_turnspeed(130)
 print frindo.set_step_time(1200)
 print frindo.set_turn_time(1000)
 
 sleep(1)
 
-def driveRobot():
+def driveRobot(): #Sleep(4) for 100cm
 	leftSpeed = 129
 	speedConst = 1.315
 	print frindo.go_diff(leftSpeed, int(round(speedConst * leftSpeed)), 1, 1)	
