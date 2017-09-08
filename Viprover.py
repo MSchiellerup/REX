8e1b5fd03cd0f563e0d967edd1bed7601aeb5dd0
Mads Skov Schiellerup <mads.schiellerup@gmail.com>
2017-09-08 09:58:04 +0200

    dads

--- Viprover.py
+++ Viprover.py
@@ -16,33 +16,11 @@ print frindo.set_turn_time(1000)
 
 sleep(1)
 
-# request to read Front IR sensor (analog sensor 0)
-#print "Front sensor = ", frindo.read_front_ir_sensor()
 
-# request to read Right IR sensor (analog sensor 1)
-#print "Right sensor = ", frindo.read_right_ir_sensor()
+leftSpeed = 128
+speedConst = 1.3
+print frindo.go_diff(leftSpeed, int(round(speedConst * leftSpeed)), 1, 1)
 
-# request to read Left IR sensor (analog sensor 2)
-#print "Left sensor = ", frindo.read_left_ir_sensor()
+sleep(3)
 
-
-
-# send a go_diff command to drive forward
-#leftSpeed = 128
-#speedConst = 1.3
-#print frindo.go_diff(leftSpeed, int(round(speedConst * leftSpeed)), 1, 1)
-
-# Wait a bit while robot moves forward
-#sleep(5)
-
-#print frindo.stop()
-
-#sleep(2)
-for x in range(1,5):
-	print frindo.right()
-	sleep(0.5)
-	print frindo.stop()
-	sleep(1)
-
-# send a stop command
-#print frindo.stop()
+print frindo.stop()
