036655f55076858be5963b7bd9386f416e9436f8
Mads Skov Schiellerup <mads.schiellerup@gmail.com>
2017-09-12 14:45:48 +0200

    sad

--- viprover.py
+++ viprover.py
@@ -41,7 +41,7 @@ def driveNumber8():
 	print frindo.go_diff(leftSpeed, int(round((speedConst * leftSpeed)/2)), 1, 1)	#hojre
 	sleep(2)
 	print frindo.go_diff(leftSpeed, int(round((speedConst * leftSpeed)/2)), 1, 1)	#hojre
-	sleep(2)
+	sleep(3)
 	print frindo.go_diff(leftSpeed/2, int(round(speedConst * leftSpeed)), 1, 1)	#venstre
 	sleep(2)
 #speedConst = 1.305
