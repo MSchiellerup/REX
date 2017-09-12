88d780081e5abd436abe5ee8ac2e6bd9bcceb1f4
Mads Skov Schiellerup <mads.schiellerup@gmail.com>
2017-09-12 14:23:46 +0200

    and

---
+++ viprover.py
@@ -0,0 +1,42 @@
+import time
+from time import sleep
+
+import robot
+
+# Create a robot object and initialize
+frindo = robot.Robot()
+
+print("Running...")
+
+# Set speed
+print frindo.set_speed(70)
+print frindo.set_turnspeed(121)
+print frindo.set_step_time(1200)
+print frindo.set_turn_time(1000)
+
+sleep(1)
+
+def driveRobot(cm): #Sleep(4) for 100cm
+	leftSpeed = 129
+	speedConst = 1.305
+	print("Driving %dcm",cm)
+	print frindo.go_diff(leftSpeed, int(round(speedConst * leftSpeed)), 1, 1)	
+	sleep(cm/25)
+
+def turnRobot(grader):
+	print frindo.right()
+	sleep((2.4/360)*grader)
+
+def driveSquared():
+	for x in range(1,5):
+		driveRobot(100)
+		turnRobot(90)
+
+speedConst = 1.305
+print frindo.go_diff(200,int(round(100*speedConst)),1,1)
+
+sleep(10)
+
+
+
+print frindo.stop()
