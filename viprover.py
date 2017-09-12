15638de0318d0614f6d46bf403bca40247d97ebc
Mads Skov Schiellerup <mads.schiellerup@gmail.com>
2017-09-12 16:04:24 +0200

    sad

--- viprover.py
+++ viprover.py
@@ -73,7 +73,7 @@ def driveAround():
 		else:
 			turnRobot(-90)
 
-print "cm: " convertDistanceToCM(frindo.read_front_ir_sensor)
+print "cm: " convertDistanceToCM(frindo.read_front_ir_sensor())
 
 
 print frindo.stop()
