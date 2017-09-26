a371d4a1d85fdff2b6ecb942dcc0b5aea3ef0811
Mads Skov Schiellerup <mads.schiellerup@gmail.com>
2017-09-26 15:41:40 +0200

    asd

--- viprover.py
+++ viprover.py
@@ -110,11 +110,11 @@ rawCapture = PiRGBArray(camera, size=camera.resolution)
 
 # Open a window
 print "1"
-WIN_RF = "Frame"
+WIN_RF = "Frame";
 print "2"
-cv2.namedWindow(WIN_RF)
+cv2.namedWindow(WIN_RF);
 print "3"
-cv2.moveWindow(WIN_RF, 100, 100)
+cv2.moveWindow(WIN_RF, 100, 100);
 print "4"
 # allow the camera to warmup
 time.sleep(0.1)
