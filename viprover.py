7383241348314991b8b94fa70589a4b6aef44a28
Mads Skov Schiellerup <mads.schiellerup@gmail.com>
2017-09-26 14:12:47 +0200

    ads

--- viprover.py
+++ viprover.py
@@ -80,26 +80,62 @@ def driveAround():
 		else:
 			turnRobot(-90)
 
-from numpy import uint8
-from numpy.random import rand
+# Example showing how to grab frames using the PiCamera module instead of OpenCV
+# import the necessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+from fractions import *
 import cv2
+ 
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+time.sleep(1) # Wait for camera
 
-xy=(512,512)
-Nf = 500
+camera.resolution = (640, 480)
+camera.framerate = 30
 
-def fpsopencv(dat):
-    tic = time()
-    for i in dat:
-        cv2.imshow('test',i)
-        cv2.waitKey(1) #integer milliseconds, 0 makes wait forever
-    cv2.destroyAllWindows()
-    return Nf / (time()-tic)
+camera.shutter_speed = camera.exposure_speed
+camera.exposure_mode = 'off'
+
+gain = camera.awb_gains
+camera.awb_mode='off'
+#gain = (Fraction(2,1), Fraction(1,1))
+#gain = (1.5, 1.5)
+camera.awb_gains = gain
+
+print "shutter_speed = ", camera.shutter_speed
+print "awb_gains = ", gain
+
+rawCapture = PiRGBArray(camera, size=camera.resolution)
+ 
+# Open a window
+WIN_RF = "Frame";
+cv2.namedWindow(WIN_RF);
+cv2.moveWindow(WIN_RF, 100       , 100);
 
 
-imgs = (rand(Nf,xy[0],xy[1])*255).astype(uint8)
-fps = fpsopencv(imgs)
+# allow the camera to warmup
+time.sleep(0.1)
+ 
+# capture frames from the camera
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+	# grab the raw NumPy array representing the image
+	image = frame.array
+ 
+	# show the frame
+	cv2.imshow(WIN_RF, image)
+	key = cv2.waitKey(4) & 0xFF
+ 
+	# clear the stream in preparation for the next frame
+	rawCapture.truncate(0)
+ 
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
 
-print("%d fps",fps)
+
+		
+
 
 
 print frindo.stop()
