d91ad26f7fe93abf8392659ff7772a64946fd32c
Mads Skov Schiellerup <mads.schiellerup@gmail.com>
2017-10-03 13:57:11 +0200

    asd

--- viprover.py
+++ viprover.py
@@ -133,12 +133,13 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 	      		best_cnt = cnt
 
 
-	  # finding centroids of best_cnt and draw a circle there
+	 # finding centroids of best_cnt and draw a circle there
   	M = cv2.moments(best_cnt)
   	cx,cy = int(M['m10']/M['m00']), int(M['m01']/M['m00'])
 	#if best_cnt>1:
 	cv2.circle(hsv,(cx,cy),10,(0,0,255),-1)
 	# show the frame
+	output = cv2.bitwise_and(image, image, mask = thresh)
 	cv2.imshow("Frame", hsv)
 	print ("x = %d, y =  %d", cx, cy)
 	#cv2.imshow('thresh',thresh2)
