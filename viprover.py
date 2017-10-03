a4321b7fd3481f6cd1f3842395798efec8de7eac
Mads Skov Schiellerup <mads.schiellerup@gmail.com>
2017-10-03 13:11:18 +0200

    asd

--- viprover.py
+++ viprover.py
@@ -86,79 +86,79 @@ def driveAround():
 		else:
 			turnRobot(-90)
 
-def detectGreenBox():
-	#def activateCam():
-		# initialize the camera and grab a reference to the raw camera capture
-	camera = PiCamera()
-	time.sleep(1) # Wait for camera
+#def detectGreenBox():
+#def activateCam():
+	# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+time.sleep(1) # Wait for camera
 
-	# Setting resolution and fr for camera
-	camera.resolution = (640, 480)
-	camera.framerate = 30
+# Setting resolution and fr for camera
+camera.resolution = (640, 480)
+camera.framerate = 30
 
-	# get the current center of the image
-  #int midx = 320
-  #int midy = 240
+# get the current center of the image
+#int midx = 320
+#int midy = 240
 
-	camera.shutter_speed = camera.exposure_speed
-	camera.exposure_mode = 'off'
+camera.shutter_speed = camera.exposure_speed
+camera.exposure_mode = 'off'
 
-	gain = camera.awb_gains
-	camera.awb_mode='off'
+gain = camera.awb_gains
+camera.awb_mode='off'
 
-	camera.awb_gains = gain
+camera.awb_gains = gain
 
-	print "shutter_speed = ", camera.shutter_speed
-	print "awb_gains = ", gain
+print "shutter_speed = ", camera.shutter_speed
+print "awb_gains = ", gain
 
-	rawCapture = PiRGBArray(camera, size=camera.resolution)
-	 
-	# Open a window
-	WIN_RF = "PiCam";
-	cv2.namedWindow(WIN_RF);
-	cv2.moveWindow(WIN_RF, 100, 100);
-
-	# define the list of boundaries
-	boundaries = [
-		([29, 86, 6], 	 [64, 255, 255]), # green
-		([17, 15, 100],  [50, 56, 200]),  # red
-		([86, 31, 4], 	 [220, 88, 50]),  # blue
-		([25, 146, 190], [62, 174, 250]), # yellow
-		([103, 86, 65],  [145, 133, 128]) # gray
-	]
-
-	greenLower = np.array([29, 86, 6])
-	greenUpper = np.array([64, 255, 255])
-
-	# allow the camera to warmup
-	time.sleep(0.1)
-
-	for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-		# grab the raw NumPy array representing the image
-		image = frame.array
-		# Convert to HSV
-		hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-		# Threshhold hsv to only get green colour
-		mask = cv2.inRange(hsv, greenLower, greenUpper)
-		# Bitwise-AND mask and original image
-    res = cv2.bitwise_and(image,image, mask = mask)
+rawCapture = PiRGBArray(camera, size=camera.resolution)
  
+# Open a window
+WIN_RF = "PiCam";
+cv2.namedWindow(WIN_RF);
+cv2.moveWindow(WIN_RF, 100, 100);
+
+# define the list of boundaries
+boundaries = [
+	([29, 86, 6], 	 [64, 255, 255]), # green
+	([17, 15, 100],  [50, 56, 200]),  # red
+	([86, 31, 4], 	 [220, 88, 50]),  # blue
+	([25, 146, 190], [62, 174, 250]), # yellow
+	([103, 86, 65],  [145, 133, 128]) # gray
+]
+
+greenLower = np.array([29, 86, 6])
+greenUpper = np.array([64, 255, 255])
+
+# allow the camera to warmup
+time.sleep(0.1)
+
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+	# grab the raw NumPy array representing the image
+	image = frame.array
+	# Convert to HSV
+	hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+	# Threshhold hsv to only get green colour
+	mask = cv2.inRange(hsv, greenLower, greenUpper)
+	# Bitwise-AND mask and original image
+  res = cv2.bitwise_and(image,image, mask = mask)
+
 # capture frames from the camera
 
+
 	
- 	
- 	#mask = cv2.inRange(image, greenLower, greenUpper)
- 	#output = cv2.bitwise_and(image, image, mask = mask)
-	# show the frame
-		cv2.imshow(WIN_RF, np.hstack([image, res]))
-		key = cv2.waitKey(4) & 0xFF
+	#mask = cv2.inRange(image, greenLower, greenUpper)
+	#output = cv2.bitwise_and(image, image, mask = mask)
+# show the frame
+	cv2.imshow(WIN_RF, np.hstack([image, res]))
+	key = cv2.waitKey(4) & 0xFF
 
-		# clear the stream in preparation for the next frame
-		rawCapture.truncate(0)
+	# clear the stream in preparation for the next frame
+	rawCapture.truncate(0)
 
-		# if the `q` key was pressed, break from the loop
-		if key == ord("q"):
-			break
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
 
 
 
