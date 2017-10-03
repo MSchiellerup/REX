d0703b5efb9dad5d8f26b3f3f6e8afdd6e29f6d4
Mads Skov Schiellerup <mads.schiellerup@gmail.com>
2017-10-03 14:20:46 +0200

    asd

--- viprover.py
+++ viprover.py
@@ -137,10 +137,10 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
   	M = cv2.moments(best_cnt)
   	cx,cy = int(M['m10']/M['m00']), int(M['m01']/M['m00'])
 	#if best_cnt>1:
-	#cv2.circle(hsv,(cx,cy),10,(0,0,255),-1)
+	cv2.circle(thresh,(cx,cy),10,(0,0,255),-1)
 	# show the frame
-	output = cv2.bitwise_and(image, image, mask = thresh)
-	cv2.imshow("Frame", np.hstack([image, output]))
+	#output = cv2.bitwise_and(image, image, mask = thresh)
+	cv2.imshow("Frame", np.hstack([image, thresh]))
 	#cv2.imshow("Frame", hsv)
 	print ("x = %d, y =  %d", cx, cy)
 	#cv2.imshow('thresh',thresh2)
