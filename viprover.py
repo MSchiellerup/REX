0ca03bfbbb8998e1dd95f7d9eeac5fece75717d0
Mads Skov Schiellerup <mads.schiellerup@gmail.com>
2017-10-03 13:44:39 +0200

    asd

--- viprover.py
+++ viprover.py
@@ -112,11 +112,12 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 	image = frame.array
 
 	blur = cv2.blur(image, (3,3))
+	hsv=cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
 	greenLower = np.array([29, 86, 6])
 	greenUpper = np.array([64, 255, 255])
 
-	thresh = cv2.inRange(blur, greenLower, greenUpper)
+	thresh = cv2.inRange(hsv, greenLower, greenUpper)
 	#thresh2 = thresh.copy()
 
 	# find contours in the threshold image
@@ -136,9 +137,9 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
   	M = cv2.moments(best_cnt)
   	cx,cy = int(M['m10']/M['m00']), int(M['m01']/M['m00'])
 	#if best_cnt>1:
-	cv2.circle(blur,(cx,cy),10,(0,0,255),-1)
+	cv2.circle(hsv,(cx,cy),10,(0,0,255),-1)
 	# show the frame
-	cv2.imshow("Frame", blur)
+	cv2.imshow("Frame", hsv)
 	print ("x = %d, y =  %d", cx, cy)
 	#cv2.imshow('thresh',thresh2)
 	key = cv2.waitKey(1) & 0xFF
