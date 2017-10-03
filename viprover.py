f505f7536a9b3b08fd5e5293a3797005a669dc92
Mads Skov Schiellerup <mads.schiellerup@gmail.com>
2017-10-03 14:21:59 +0200

    asd

--- viprover.py
+++ viprover.py
@@ -140,7 +140,7 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 	cv2.circle(thresh,(cx,cy),10,(0,0,255),-1)
 	# show the frame
 	#output = cv2.bitwise_and(image, image, mask = thresh)
-	cv2.imshow("Frame", np.hstack([image, thresh]))
+	cv2.imshow("Frame", np.hstack([image, hsv]))
 	#cv2.imshow("Frame", hsv)
 	print ("x = %d, y =  %d", cx, cy)
 	#cv2.imshow('thresh',thresh2)
